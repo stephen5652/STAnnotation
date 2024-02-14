87236390460337eb1044958a196b7349ebaa9c0b
coder <coder@gmail.com>
2024-02-15 01:07:49 +0800

    feat: add project

---
+++ Example/STAnnotation_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STAnnotation_Example
+//
+//  Created by coder on 2024/02/15.
+//
+
+import UIKit
+import STAnnotation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
